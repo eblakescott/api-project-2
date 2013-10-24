dca86f563b772caba4dc35a61272bf7b5d515fb3
Ed Scott <eblakescott@gmail.com>
2013-10-24 18:05:05 -0500

    trying to get all comments using offset option of api

--- server.js
+++ server.js
@@ -2,12 +2,28 @@
 
 var request = require('request');
 var fs = require("fs");
-var myFileName = 'comments.txt'
+var myFileName = 'comments.txt';
+var apiKey = '22782cf7bd7c5904640c86397bf65764:19:54772968';
 
-//request nytimes comments for story at http://www.nytimes.com/2013/07/05/opinion/krugman-e-pluribus-unum.html, sorted by editor's selection
+//request nytimes comments for story at http://www.nytimes.com/2013/07/05/opinion/krugman-e-pluribus-unum.html, sorted by social ranking mechanism
 
-request('http://api.nytimes.com/svc/community/v2/comments/url/exact-match.xml?url=http%3A%2F%2Fwww.nytimes.com%2F2013%2F07%2F05%2Fopinion%2Fkrugman-e-pluribus-unum.html&sort=editors-selection&api-key=22782cf7bd7c5904640c86397bf65764:19:54772968').pipe(fs.createWriteStream('comments.txt')), function (error, response, body) {
+
+request('http://api.nytimes.com/svc/community/v2/comments/url/exact-match?url=http%3A%2F%2Fwww.nytimes.com%2F2013%2F07%2F05%2Fopinion%2Fkrugman-e-pluribus-unum.html&sort=recommended&api-key=' + apiKey).pipe(fs.createWriteStream('comments.txt')), function (error, response, body) {
   if (!error && response.statusCode == 200) {
     console.log('file successfully written') 
   }
-};
+}
+
+request('http://api.nytimes.com/svc/community/v2/comments/url/exact-match?url=http%3A%2F%2Fwww.nytimes.com%2F2013%2F07%2F05%2Fopinion%2Fkrugman-e-pluribus-unum.html&offset=25&sort=recommended&api-key=' + apiKey).pipe(fs.createWriteStream('comments.txt')), function (error, response, body) {
+  if (!error && response.statusCode == 200) {
+    console.log('file successfully written') 
+  }
+}
+
+request('http://api.nytimes.com/svc/community/v2/comments/url/exact-match?url=http%3A%2F%2Fwww.nytimes.com%2F2013%2F07%2F05%2Fopinion%2Fkrugman-e-pluribus-unum.html&offset=50&sort=recommended&api-key=' + apiKey).pipe(fs.createWriteStream('comments.txt')), function (error, response, body) {
+  if (!error && response.statusCode == 200) {
+    console.log('file successfully written') 
+  }
+}
+
+;
