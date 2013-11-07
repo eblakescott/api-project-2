c7c55c460f2ef345ad3153fbcfd5b97ede99694b
Ed Scott <eblakescott@gmail.com>
2013-11-07 15:56:50 -0600

    trying to combine request output and comment body manipulation

--- server.js
+++ server.js
@@ -2,13 +2,17 @@
 //http://nodejs.org/api/fs.html
 
 var request = require('request');
-var fs = require("fs");
+var fs = require('fs');
 var myFileName = 'comments.txt';
+var url = 'http://api.nytimes.com/svc/community/v2/comments/url/exact-match?url=http%3A%2F%2Fwww.nytimes.com%2F2013%2F07%2F05%2Fopinion%2Fkrugman-e-pluribus-unum.html';
+var offset = '&offset=0';
+var sorting = '&sort=recommended';
 var apiKey = '22782cf7bd7c5904640c86397bf65764:19:54772968';
 
 //request NYTimes comments for story at http://www.nytimes.com/2013/07/05/opinion/krugman-e-pluribus-unum.html, sorted by social ranking mechanism. Each function gets 25 comments; the first gets the first 25, the second gets 26-50, etc., for a total of 246 comments that have at least one "like" vote. Functions execute at ten-second intervals.
 
 function getComments1 () {
+
 request('http://api.nytimes.com/svc/community/v2/comments/url/exact-match?url=http%3A%2F%2Fwww.nytimes.com%2F2013%2F07%2F05%2Fopinion%2Fkrugman-e-pluribus-unum.html&sort=recommended&api-key=' + apiKey).pipe(fs.createWriteStream('comments' + '1-25' + '.js')), function (error, response, body) {
   if (!error && response.statusCode == 200) {
     console.log('file successfully written') 
